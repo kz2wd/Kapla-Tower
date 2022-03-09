4961192886ea67035b3095f805ee8d48a4d6c14f
Youri <itrotky@gmail.com>
2022-03-09 12:03:10 +0100

    progress on visu

--- tests/CV2_Personnal_Utils.py
+++ tests/CV2_Personnal_Utils.py
@@ -42,3 +42,16 @@ def assemble_frames(frame1, frame2, shift_x=None, shift_y=None, default_color=(0
 
     return result
 
+
+def draw_cross(img, center, length, width, color):
+    center = list(map(int, center))
+    img[max(center[0] - length, 0): min(center[0] + length, img.shape[0]),
+        max(center[1] - width, 0): min(center[1] + width, img.shape[1])] = color
+    img[max(center[0] - width, 0): min(center[0] + width, img.shape[0]),
+        max(center[1] - length, 0): min(center[1] + length, img.shape[1])] = color
+
+
+def draw_grid(frame, space, color):
+    for x in range(0, frame.shape[0], space):
+        for y in range(0, frame.shape[1], space):
+            draw_cross(frame, (x, y), 3, 3, color)
--- tests/kapla_trigger.py
+++ tests/kapla_trigger.py
@@ -1,11 +1,13 @@
+import time
+
 import cv2
 import numpy as np
 import math
-from CV2_Personnal_Utils import assemble_frames
+from CV2_Personnal_Utils import assemble_frames, draw_grid
 
 
 def extract_matrix(frame, start_x, start_y, size_x, size_y):
-    w, h = frame.shape
+    w, h = frame.shape[:2]
 
     end_x = min(start_x + size_x, w)
     end_y = min(start_y + size_y, h)
@@ -13,33 +15,73 @@ def extract_matrix(frame, start_x, start_y, size_x, size_y):
     return np.array(frame[start_y: end_y, start_x: end_x])
 
 
-def apply_houghP(source, display=None, shift=None):
-    if display is None:
-        display = source
-    if shift is None:
-        shift = (0, 0)
+def apply_houghP(source):
     ret, bin_img = cv2.threshold(source, 150, 255, cv2.THRESH_BINARY_INV)
     edges = cv2.Canny(bin_img, 50, 150, apertureSize=3)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 70, minLineLength=20, maxLineGap=15)
-
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            x1 += shift[0]
-            x2 += shift[0]
-            y1 += shift[1]
-            y2 += shift[1]
-            cv2.line(display, (x1, y1), (x2, y2), 255, 2)
-            cv2.circle(display, (x1, y1), 4, 255, 5)
-            cv2.circle(display, (x2, y2), 4, 255, 5)
+    # blur = cv2.GaussianBlur(edges, (3, 3), 0)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 20, minLineLength=50, maxLineGap=15)
 
     return lines, edges
 
 
-def get_kapla_pos(lines):
-    print(lines[0])
-    longest = max(map(lambda x: math.sqrt(pow(x[0][2] - x[0][1], 2) + pow(x[0][3] - x[0][1], 2)), lines))
-    print(longest)
+def print_lines(lines, display, color, shift=None):
+    if shift is None:
+        shift = (0, 0)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = apply_shift(*line[0], shift)
+            cv2.line(display, (x1, y1), (x2, y2), color, 2)
+            cv2.circle(display, (x1, y1), 4, color, 5)
+            cv2.circle(display, (x2, y2), 4, color, 5)
+
+
+def apply_shift(x1, y1, x2, y2, shift):
+    x1 += shift[0]
+    x2 += shift[0]
+    y1 += shift[1]
+    y2 += shift[1]
+    return x1, y1, x2, y2
+
+
+
+def has_similar(lines, line, epsilon=5):
+    for l in lines:
+        # print(abs(l[0][0] - line[0][0]), abs(l[0][1] - line[0][1]), abs(l[0][2] - line[0][2]), abs(l[0][3] - line[0][3]))
+        if abs(l[0][0] - line[0][0]) < epsilon or abs(l[0][1] - line[0][1]) < epsilon \
+                or abs(l[0][2] - line[0][2]) < epsilon or abs(l[0][3] - line[0][3]) < epsilon:
+            return True
+    return False
+
+
+def simplify_lines(lines, epsilon=5):
+    for i in range(len(lines) - 1, 0, -1):
+        if has_similar(lines[:i], lines[i], epsilon):
+            lines = np.delete(lines, i, 0)
+    return lines
+
+
+def get_kapla_pos(lines, display, shift=None):
+    if shift is None:
+        shift = (0, 0)
+    lines_distance = {math.sqrt(pow(x[0][2] - x[0][0], 2) + pow(x[0][3] - x[0][1], 2)): x for x in lines}
+
+    dist_max = max(lines_distance)
+    longest = lines_distance[dist_max]
+    # print(dist_max)
+    # print(f"ratio {dist_max / 70} pix/mm")
+
+
+    x1, y1, x2, y2 = apply_shift(*longest[0], shift)
+    cv2.line(display, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+    average_line = np.mean(lines, 0)
+    x1, y1, x2, y2 = apply_shift(*map(int, average_line[0]), shift)
+    cv2.line(display, (x1, y1), (x2, y2), (0, 0, 255), 4)
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -50,23 +92,38 @@ if __name__ == "__main__":
         ret, frame = cam.read()
 
         # working_frame = frame[:, 270:490]
-        frame = frame[:1280, :720]
+        # frame = frame[:1280, :720]
         working_frame = np.copy(frame)
+
+        # draw_grid(frame, 50, (0, 255, 255))
+        # draw_grid(frame, 100, (255, 0, 255))
 
         working_frame = cv2.cvtColor(working_frame, cv2.COLOR_BGR2GRAY)
 
-        # working_frame = extract_matrix(working_frame, 200, 200, 100, 100)
+        shift = (275, 150)
+        # shift = (0, 0)
+        working_frame = extract_matrix(working_frame, shift[0], shift[1], 150, 100)
+        # working_frame = extract_matrix(working_frame, shift[0], shift[1], 300, 300)
 
-        lines, edges = apply_houghP(working_frame, frame, (0, 0))
+        lines, edges = apply_houghP(working_frame)
 
         if lines is not None:
-            get_kapla_pos(lines)
+            # before_clean = len(lines)
+            # print_lines(lines, frame, (75, 75, 75), shift)
+            # lines = simplify_lines(lines, 5)
+            # print(f"{before_clean} => {len(lines)}")
+            print_lines(lines, frame, (250, 0, 0), shift)
+            get_kapla_pos(lines, frame, shift)
+
+        else:
+            print("No line")
 
         display = assemble_frames(frame, cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB))
 
         cv2.imshow('frame', display)
         if cv2.waitKey(1) and 0xFF == ord('q'):
             break
+        time.sleep(1)
 
     cam.release()
     cv2.destroyAllWindows()
